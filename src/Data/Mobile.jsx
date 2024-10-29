2a4c9166c546c8952a17b30a69defdebf80b6353
veturu praneeth <vpraneeth143@gmail.com>
2024-10-29 09:35:32 +0530

    new

--- src/Data/Mobile.jsx
+++ src/Data/Mobile.jsx
@@ -42,6 +42,61 @@ export const Mobile = [
         price: 25000,
         Image:'https://m.media-amazon.com/images/I/416t0eHB3SL._SX300_SY300_QL70_FMwebp_.jpg',
     },
+    {
+        id: 8,
+        title: "realme",  
+        price: 25000,
+        Image:"https://m.media-amazon.com/images/I/714oelPJK0L._SY879_.jpg",
+    },
+    {
+        id: 9,
+        title: "samsung",  
+        price: 164000,
+        Image:"https://m.media-amazon.com/images/I/61BgEfmZC8L._SX679_.jpg",
+    },
+    {
+        id: 10,
+        title: "samsung",  
+        price: 90000,
+        Image:"https://m.media-amazon.com/images/I/61JaMFIFUNL._SX679_.jpg",
+
+    },
+    {
+        id: 11,
+        title: "iphone",  
+        price: 119000,
+        Image:"https://m.media-amazon.com/images/I/61JvFLHZ6NL._SX679_.jpg",
+    },
+    {
+        id: 12,
+        title: "iqoo",  
+        price: 66000,
+        Image:"https://m.media-amazon.com/images/I/71d7rfSl0wL._SX679_.jpg",
+    },
+    {
+        id: 13,
+        title: "vivo",  
+        price: 37000,
+        Image:"https://m.media-amazon.com/images/I/61Mr3os9ZQL._SX679_.jpg",
+    },
+    {
+        id: 14,
+        title: "realme",  
+        price: 8000,
+        Image:"https://m.media-amazon.com/images/I/71r0Ysx+oVL._SX679_.jpg",
+    },
+    {
+        id: 15,
+        title: "motorola",  
+        price: 17500,
+        Image:"https://m.media-amazon.com/images/I/715Uq2DHIWL._SY879_.jpg",
+    },
+    {
+        id: 16,
+        title: "samsung",  
+        price: 12000,
+        Image:"https://m.media-amazon.com/images/I/81oKOk65TgL._SX679_.jpg",
+    }
 ];
 
 export default Mobile;  
--- src/Pages/Newmobile.jsx
+++ src/Pages/Newmobile.jsx
@@ -8,7 +8,7 @@ function MobileList() {
         <div className='mobile' style={{display:'flex', flexWrap:'wrap',gap:'3%',padding:'3px'}}>
             {MobileData.map((item) => (
                 <div className='new'  style={{border:'2px solid black', margin:'10px'}}>
-                    <h2>Title: {item.title}</h2>
+                    <h2 style={{color:'black'}}>Title: {item.title}</h2>
                     <img src={item.Image} alt={item.title} style={{height:'350px', width:'300px'}} />
                     <p className='hero' style={{color:'black'}}>Price: {item.price}</p>
                     <Button className='btn btn-warning'>
